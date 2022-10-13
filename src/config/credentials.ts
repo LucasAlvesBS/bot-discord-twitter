1ae392bd3846b215b11e43429fc0b26fdb054b67
Lucas Alves <lucasalvesbs6@gmail.com>
2022-10-12 21:29:31 -0300

    feat: connect bot with discord and add credentials

---
+++ src/config/credentials.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const credentials = {
+  discordToken: process.env.DISCORD_TOKEN,
+  discordChannelId: process.env.DISCORD_CHANNEL_ID || '',
+  bearerToken: process.env.TWITTER_BEARER_TOKEN || '',
+  username: process.env.TWITTER_USERNAME,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello Dev!');
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import { credentials } from './config/credentials';
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  partials: [Partials.Channel],
+});
+
+if (!credentials.discordToken) {
+  throw new Error('Token invalid');
+}
+
+client.login(credentials.discordToken);
+
+client.on('ready', () => {
+  console.log('Discord ready to start');
+});
